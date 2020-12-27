7bc102c00891c2c7cec20f80a79e5f7cc8dec7da
Ivan Stanojevic <ivanstan@gmail.com>
2020-12-27 21:50:03 +0100

    add tests

--- tests/TleModelTest.php
+++ tests/TleModelTest.php
@@ -3,7 +3,9 @@
 namespace App\Tests;
 
 use Ivanstan\Tle\Helper\SampleTleHelper;
+use Ivanstan\Tle\Model\Tle;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 
 final class TleModelTest extends TestCase
 {
@@ -44,7 +46,22 @@ final class TleModelTest extends TestCase
         static::assertEquals(15.57860024, $tle->meanMotion(), 'Assert correct mean motion');
         static::assertEquals(334.0891, $tle->raan(), 'Assert correct RAAN');
         static::assertEquals(18321.21573649, $tle->epoch(), 'Assert correct epoch');
-        static::assertEquals('1 43550U 98067NY  18321.21573649  .00013513  00000-0  18402-3 0  9990', $tle->getLine1(), 'Assert correct first line');
-        static::assertEquals('2 43550  51.6389 334.0891 0005785  67.0956 293.0647 15.57860024 19804', $tle->getLine2(), 'Assert correct second line');
+        static::assertEquals(
+            '1 43550U 98067NY  18321.21573649  .00013513  00000-0  18402-3 0  9990',
+            $tle->getLine1(),
+            'Assert correct first line'
+        );
+        static::assertEquals(
+            '2 43550  51.6389 334.0891 0005785  67.0956 293.0647 15.57860024 19804',
+            $tle->getLine2(),
+            'Assert correct second line'
+        );
+
+        $class = new ReflectionClass(Tle::class);
+        $method = $class->getMethod('getLineByNumber');
+        $method->setAccessible(true);
+
+        $this->expectExceptionMessage('Invalid line number 3');
+        $method->invokeArgs($tle, [3]);
     }
 }
